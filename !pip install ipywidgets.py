cd3cbed9094da845cc6f042674de3392cfb14984
Md Zobayer Hossain <mdzobaye@ualberta.ca>
2026-02-04 23:19:02 -0700

    s

---
+++ !pip install ipywidgets.py
@@ -0,0 +1,11 @@
+!pip install ipywidgets
+import pandas as pd
+import numpy as np
+import math
+import ipywidgets as widgets
+
+##Seaborn for fancy plots. 
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.rcParams["figure.figsize"] = (8,8)
